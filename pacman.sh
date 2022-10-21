06e4b46d4f65609b1101c06d19e6344955ab7312
mysterek_ <59736397+mysterek1337@users.noreply.github.com>
2022-10-21 18:06:51 +0200

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,7 +1,7 @@
 pacman -Sy
 pacman -S sudo nano --noconfirm
-pacman -S wget --nocofirm
-pacman -S xorg plasma plasma-wayland-session kde-applications --noconfirm
+pacman -S --needed xorg sddm --noconfirm
+pacman -S --needed plasma kde-applications --noconfirm
 systemctl enable sddm.service
 systemctl enable NetworkManager.service
 shutdown now
